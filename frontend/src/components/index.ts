953a0f42982a733b710ac250d2d8ed0863ba0efa
Brett Beaulieu <user@example.com>
2024-09-03 09:36:56 -0400

    fix index references

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,8 +2,5 @@
 
 export { DateMenu } from './DateMenu/DateMenu';
 export { InteractiveAreaChart } from './InteractiveAreaChart/InteractiveAreaChart'
-export { LinksGroup } from './NavbarLinksGroup/NavbarLinksGroup';
-export { NavbarLinksGroup } from './NavbarLinksGroup/NavbarLinksGroup';
-export { NavbarNested } from './NavbarNested/NavbarNested';
 export { StatsGrid } from './StatsGrid/StatsGrid';
 export { ThemeToggle } from './ThemeToggle/ThemeToggle';
